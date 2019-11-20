bc08c4a75cdc9b4af379df187ec6a9c694e788c0
muhamaderi <Muhamaderi2001@gmail.com>
2019-11-20 12:59:05 +0700

    JAVA

---
+++ Latihan 10/src/latihan/pkg10/Latihan10.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg10;
+
+/**
+ *
+ * @author MUHAMAD ERI
+ * NAMA                 : MUHAMAD ERI RIZKI HADI A
+ * KELAS                : TI -1B
+ * NIM                  : A2.1900112
+ * Deskripsi Program    : Logika
+ */
+public class Latihan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
---
+++ Latihan 6/src/latihan/pkg6/Mamalia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg6;
+
+/**
+ *
+ * @author MUHAMAD ERI 
+ * NAMA                 : MUHAMAD ERI RIZKI HADI A
+ * KELAS                : TI -1B
+ * NIM                  : A2.1900145
+ */
+public class Mamalia {
+    // Variabel jumlahKambing dideklarasika sebagai statik
+    public static int jumlahKambing;
+    
+}
